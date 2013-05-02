8310c4effa1df65f570d52f0de8cfe3d6a50408f
Shuotao Xu <shuotao@shuotao-ThinkPad-W530.(none)>
2013-05-01 23:02:24 -0400

    cmd scheduler

---
+++ sw_model/linkBlocks.h
@@ -0,0 +1,21 @@
+#ifndef LINK_BLOCKS_H
+#define LINK_BLOCKS_H
+
+
+#include <iostream>
+#include <unistd.h>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+#include <stdint.h> 
+
+
+void scheduleCmds();
+
+//void dumpCmdEntry(CmdEntry cmdEntry);
+//void dumpTableMetas();
+
+
+
+#endif //LINK_BLOCKS_H
